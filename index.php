cd527d5541e374472deebc5a6e2ba66df26f348b
rechantex <tikasetara04@gmail.com>
2023-04-27 22:53:57 +0700

    index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
+    $nama_kamu = "Echa";
+    $umur = 5;
     // tanda petik dua
-    echo "Hello word";
+    /*
+    echo "Hello world, Welcome ".$nama_kamu;
+    echo " Umur Kamu ".$umur;
+    echo " tahun";
     echo "<br/>";
+    */
+    echo "Hello World, Welcome ".$nama_kamu." Umur Kamu ".$umur." tahun";
+    echo "<br/>"
+    echo "Berarti $nama_kamu lahir di tahun ".2023-$umur;
     // tanda petik satu
-    echo 'Hello word';
+    echo 'Hello world';
 
 ?>
